2ac0128c1e3fd1452a696e588d0343a11c8e59f7
Jack Cook <hello@jackcook.nyc>
2019-06-29 22:17:42 -0700

    Implemented more keystrokes, delete, and saving text

--- frontend/src/components/WritingView.js
+++ frontend/src/components/WritingView.js
@@ -14,9 +14,10 @@ class WritingView extends Component {
 
     this.state = {
       caretVisible: true,
+      commandDown: false,
       optionDown: false,
-      textLeft: "",
-      textRight: ""
+      textLeft: localStorage.getItem("textLeft") || "",
+      textRight: localStorage.getItem("textRight") || ""
     };
 
     this.handleKeyDown = this.handleKeyDown.bind(this);
@@ -41,6 +42,8 @@ class WritingView extends Component {
   handleKeyDown(e) {
     console.log(e);
 
+    var i = 0;
+
     if (e.key.length === 1) {
       this.setState({
         textLeft: this.state.textLeft + e.key
@@ -51,7 +54,7 @@ class WritingView extends Component {
         let inSpaces = true;
 
         for (var i = this.state.textLeft.length - 1; i >= 0; i--) {
-          if (this.state.textLeft.charAt(i) !== " ") {
+          if (this.state.textLeft.charAt(i) !== " " && this.state.textLeft.charAt(i) !== "\n") {
             inSpaces = false;
           } else if (!inSpaces) {
             break;
@@ -62,6 +65,20 @@ class WritingView extends Component {
           textLeft: this.state.textLeft.substring(0, i + 1),
           textRight: this.state.textLeft.substring(i + 1, this.state.textLeft.length) + this.state.textRight
         });
+      } else if (this.state.commandDown) {
+        e.preventDefault();
+
+        if (this.state.textLeft.indexOf("\n") === -1) {
+          this.setState({
+            textLeft: "",
+            textRight: this.state.textLeft + this.state.textRight
+          });
+        } else {
+          this.setState({
+            textLeft: this.state.textLeft.substring(0, this.state.textLeft.lastIndexOf("\n") + 1),
+            textRight: this.state.textLeft.substring(this.state.textLeft.lastIndexOf("\n") + 1, this.state.textLeft.length) + this.state.textRight
+          });
+        }
       } else {
         this.setState({
           textLeft: this.state.textLeft.substring(0, this.state.textLeft.length - 1),
@@ -73,8 +90,8 @@ class WritingView extends Component {
       if (this.state.optionDown) {
         let inSpaces = true;
 
-        for (var j = 0; j < this.state.textRight.length; j++) {
-          if (this.state.textRight.charAt(j) !== " ") {
+        for (i = 0; i < this.state.textRight.length; i++) {
+          if (this.state.textRight.charAt(i) !== " " && this.state.textRight.charAt(i) !== "\n") {
             inSpaces = false;
           } else if (!inSpaces) {
             break;
@@ -82,9 +99,23 @@ class WritingView extends Component {
         }
 
         this.setState({
-          textLeft: this.state.textLeft + this.state.textRight.substring(0, j),
-          textRight: this.state.textRight.substring(j, this.state.textRight.length)
+          textLeft: this.state.textLeft + this.state.textRight.substring(0, i),
+          textRight: this.state.textRight.substring(i, this.state.textRight.length)
         });
+      } else if (this.state.commandDown) {
+        e.preventDefault();
+
+        if (this.state.textRight.indexOf("\n") === -1) {
+          this.setState({
+            textLeft: this.state.textLeft + this.state.textRight,
+            textRight: ""
+          });
+        } else {
+          this.setState({
+            textLeft: this.state.textLeft + this.state.textRight.substring(0, this.state.textRight.indexOf("\n")),
+            textRight: this.state.textRight.substring(this.state.textRight.indexOf("\n"), this.state.textRight.length)
+          });
+        }
       } else {
         this.setState({
           textLeft: this.state.textLeft + this.state.textRight.charAt(0),
@@ -93,13 +124,43 @@ class WritingView extends Component {
       }
     } else if (e.keyCode === 8) {
       // Delete
-      this.setState({
-        textLeft: this.state.textLeft.substring(0, this.state.textLeft.length - 1)
-      });
+      if (this.state.optionDown) {
+        let inSpaces = true;
+
+        for (i = this.state.textLeft.length - 1; i >= 0; i--) {
+          if (this.state.textLeft.charAt(i) !== " ") {
+            inSpaces = false;
+          } else if (!inSpaces) {
+            break;
+          }
+        }
+
+        this.setState({
+          textLeft: this.state.textLeft.substring(0, i + 1)
+        });
+      } else if (this.state.commandDown) {
+        this.setState({
+          textLeft: ""
+        });
+      } else {
+        this.setState({
+          textLeft: this.state.textLeft.substring(0, this.state.textLeft.length - 1)
+        });
+      }
     } else if (e.keyCode === 18) {
       // Option
       this.setState({
         optionDown: true
+      });
+    } else if (e.keyCode === 91) {
+      // Command
+      this.setState({
+        commandDown: true
+      });
+    } else if (e.keyCode === 13) {
+      // Enter
+      this.setState({
+        textLeft: this.state.textLeft + "\n"
       });
     }
 
@@ -111,6 +172,11 @@ class WritingView extends Component {
       // Option
       this.setState({
         optionDown: false
+      });
+    } else if (e.keyCode === 91) {
+      // Command
+      this.setState({
+        commandDown: false
       });
     }
   }
@@ -143,6 +209,9 @@ class WritingView extends Component {
         caretVisible: !this.state.caretVisible
       });
     }, 500);
+
+    localStorage.setItem("textLeft", this.state.textLeft);
+    localStorage.setItem("textRight", this.state.textRight);
   }
 
   render() {
